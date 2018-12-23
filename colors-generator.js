207a80cc311ae0450f0044bec999a1653bb961b9
Belov <belov@Yevgeniys-MacBook-Pro.local>
2018-12-23 12:35:38 +0200

    fix module export

--- colors-generator.js
+++ colors-generator.js
@@ -1,5 +1,4 @@
 const Color = require('color');
-// const fs = require('fs');
 var colorsList = {};
 const colors_3 = [
     'blue',
@@ -34,37 +33,33 @@ const shades_4 = [
     '-darker',
     '-darkest'
 ];
+
 function generateShades(color, shadesNumber, counter, colorsNumber) {
     let step = 100 / shadesNumber;
     for (let i = 1; i < shadesNumber + 1; i++) {
         let className = '';
-    if (colorsNumber === 3) {
-        className = colors_3[counter - 1] + shades_4[i - 1];
-    } else
-    if (colorsNumber === 6) {
-        className = colors_6[counter - 1] + shades_4[i - 1];
-    } else
-    if (colorsNumber === 12) {
-        className = colors_12[counter - 1] + shades_4[i - 1];
-    } else {
-        className = colorsNumber + '_' + counter + shades_4[i - 1];
-    }
+        if (colorsNumber === 3) {
+            className = colors_3[counter - 1] + shades_4[i - 1];
+        } else
+        if (colorsNumber === 6) {
+            className = colors_6[counter - 1] + shades_4[i - 1];
+        } else
+        if (colorsNumber === 12) {
+            className = colors_12[counter - 1] + shades_4[i - 1];
+        } else {
+            className = colorsNumber + '_' + counter + shades_4[i - 1];
+        }
         colorsList[className] = (color.darken((i - 1) * step / 100).hex());
     }
 }
+var exports = module.exports = {};
 
-function generateColors(colorsNumber, shadesNumber) {
+exports.generateColors = function (colorsNumber, shadesNumber) {
     let firstColor = Color('#ff0000');
     let angle = 360 / colorsNumber;
     for (let i = 1; i < colorsNumber + 1; i++) {
         let color = firstColor.rotate(angle * i);
         generateShades(color, shadesNumber, i, colorsNumber);
     }
-    console.log(colorsList);
-    
     return colorsList;
-    // fs.writeFile("colors_2.json", JSON.stringify(colorsList), function (err) {
-    //     console.log("The file was saved!");
-    // });
-}
-generateColors(6, 4);
+} 
